2414d7aa7e1309acd7cc8e313282e3da9fce030c
EyalPaz-700 <eyalp700@gmail.com>
2023-10-18 11:20:12 +0300

    initial memory game

---
+++ js/memory.js
@@ -0,0 +1,75 @@
+const cards = document.querySelectorAll(".card");
+const startGameBtn = document.getElementById("start-game");
+
+startGameBtn.addEventListener("click",startGame)
+
+let matched = 0;
+let cardOne, cardTwo;
+let disableDeck = false;
+
+function flipCard({target: clickedCard}) {
+    if(cardOne !== clickedCard && !disableDeck) {
+        clickedCard.classList.add("flip");
+        if(!cardOne) {
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+        disableDeck = true;
+        let cardOneImg = cardOne.querySelector(".back-view img").src,
+        cardTwoImg = cardTwo.querySelector(".back-view img").src;
+        matchCards(cardOneImg, cardTwoImg);
+    }
+}
+
+function matchCards(img1, img2) {
+    if(img1 === img2) {
+        matched++;
+        if(matched == 8) {
+            endGame()
+        }
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = "";
+        return disableDeck = false;
+    }
+    setTimeout(() => {
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+
+    setTimeout(() => {
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = "";
+        disableDeck = false;
+    }, 1200);
+}
+
+function shuffleCards() {
+    matched = 0;
+    disableDeck = false;
+    cardOne = cardTwo = "";
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+    cards.forEach((card, i) => {
+        card.classList.remove("flip");
+        let imgTag = card.querySelector(".back-view img");
+        imgTag.src = `../media/img-${arr[i]}.png`;
+        card.addEventListener("click", flipCard);
+    });
+}
+function endGame(){
+    cards.forEach(card => {
+        card.onclick = undefined
+    });
+}
+
+function startGame(){
+    matched = 0
+    shuffleCards();
+    cards.forEach(card => {
+        card.addEventListener("click", flipCard);
+    });
+}
+
+
